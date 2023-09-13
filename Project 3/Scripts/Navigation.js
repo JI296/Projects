17715714cd0387a8f2874c5f7eb8d6c7c9c03b6d
JI296 <144953487+JI296@users.noreply.github.com>
2023-09-13 13:23:38 -0700

    Add files via upload

---
+++ Project 3/Scripts/Navigation.js
@@ -0,0 +1,16 @@
+function addValueToPassword(button)
+{
+$("#btnEnter").click(function()
+{
+    var password = getpassword();
+    if(document.geteElementById("passcode").value==password)
+    {
+        
+    }
+
+
+
+}
+
+
+}
